11b16173562fe3aefd88c77dc45caf1cbb095367
gauquier <jonasdg@hotmail.be>
2018-02-14 17:03:03 +0100

    Part2
    * part 1
    
    * added forms and enquiry entity
    
    * validators, forms and emailing (part 2)
    
    * updated after review

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -9,6 +9,9 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
+use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends Controller
 {
@@ -20,5 +23,32 @@ class PageController extends Controller
     public function aboutAction()
     {
         return $this->render('@BloggerBlog/Page/about.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Contact enquiry from symblog')
+                ->setFrom('enquiries@symblog.co.uk')
+                ->setTo($this->getParameter('blogger_blog.emails.contact_email'))
+                ->setBody($this->renderView('@BloggerBlog/Page/contactEmail.txt.twig', array('enquiry' => $enquiry)));
+            $this->get('mailer')->send($message);
+
+            $this->addFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+            // Redirect - This is important to prevent users re-posting
+            // the form if they refresh the page
+            return $this->redirectToRoute('blogger_blog_contact');
+        }
+
+        return $this->render('@BloggerBlog/Page/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
