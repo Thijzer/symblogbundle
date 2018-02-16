80410814eb87e13b5e4ee1a0622762f9c1539830
gauquier <jonasdg@hotmail.be>
2018-02-16 17:24:52 +0100

    Part 4.1 changed repositories to own directory, added migrations

--- src/Blogger/BlogBundle/Repository/ArticleRepository.php
+++ src/Blogger/BlogBundle/Repository/ArticleRepository.php
@@ -6,17 +6,7 @@ use Doctrine\ORM\EntityRepository;
 
 class ArticleRepository extends EntityRepository
 {
-    public function getAllArticles()
-    {
-        $qb = $this->createQueryBuilder('a')
-            ->orderBy('a.created', 'DESC')
-            ->getQuery()
-        ;
-
-        return $qb->execute();
-    }
-
-    public function getLatestArticles($limit = null)
+    public function getAllArticles($limit = null)
     {
         $qb = $this->createQueryBuilder('a')
             ->select('a')
@@ -24,8 +14,9 @@ class ArticleRepository extends EntityRepository
             ->getQuery()
         ;
 
-        if (false === is_null($limit))
+        if ($limit !== null) {
             $qb->setMaxResults($limit);
+        }
 
         return $qb->execute();
     }
